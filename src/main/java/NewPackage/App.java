94d150576013ee6350e488923c9eb417d781fb8b
Sanjay <sanjay.mundu2000@gmail.com>
2018-09-10 23:45:14 +0530

    First commit

---
+++ src/main/java/NewPackage/App.java
@@ -0,0 +1,13 @@
+package NewPackage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
